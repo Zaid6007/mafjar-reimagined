c6417133ef061a05bde8ccf2a6291006faa5fff6
Zaid6007 <gaming5number@gmail.com>
2026-06-15 15:43:09 +0100

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,10 +16,7 @@ import Checkout from "./pages/Checkout";
 import Auth from "./pages/Auth";
 import Account from "./pages/Account";
 import NotFound from "./pages/NotFound";
-import { HashRouter } from "react-router-dom";
 
-<HashRouter>
-</HashRouter>
 const queryClient = new QueryClient();
 
 const App = () => (
